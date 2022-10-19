0696f66525de0a03a3d194d24dcf6f6f15d88970
Donal Tuohy <donaltuohy@gmail.com>
2022-10-19 22:35:17 +0100

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,28 +4,10 @@ set -e
 api_url="${INPUT_WEBHOOK}"
 
 body=$(cat <<EOF
-{   
-    "blocks": [
-        {
-        "type": "header",
-        "text": {
-            "type": "plain_text",
-            "text": "${INPUT_SERVICE_NAME} Deployment ${INPUT_ACTION} - ${INPUT_REGION}",
-            "emoji": true
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*REGION:* ${INPUT_REGION}"
-            }
-        }
-    ]
-}
+'{"blocks": [{"type": "header","text": {"type": "plain_text","text": "${INPUT_SERVICE_NAME} Deployment ${INPUT_ACTION} - ${INPUT_REGION}","emoji": true}},{"type": "section","text": {"type": "mrkdwn","text": "*REGION:* ${INPUT_REGION}"}}]}'
 EOF
 )
 
-result=$(curl -X POST "${api_url}" -d 'body'
+result=$(curl -X POST "${api_url}" -d ${body}
 
 echo "::set-output name=pokemon_name::$pokemon_name"
