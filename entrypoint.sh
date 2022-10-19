74b4d96bd7059e49409093d91416dd2fa1cd60a5
Donal Tuohy <donaltuohy@gmail.com>
2022-10-19 22:16:52 +0100

    Add action

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+api_url="${INPUT_WEBHOOK}"
+
+body=$(cat <<EOF
+{   
+    "blocks": [
+        {
+        "type": "header",
+        "text": {
+            "type": "plain_text",
+            "text": "${INPUT_SERVICE_NAME} Deployment ${INPUT_ACTION} - ${INPUT_REGION}",
+            "emoji": true
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*REGION:* ${INPUT_REGION}"
+            }
+        }
+    ]
+}
+EOF
+)
+
+result=$(curl -X POST "${api_url}" -d 'body'
+
+echo "::set-output name=pokemon_name::$pokemon_name"
